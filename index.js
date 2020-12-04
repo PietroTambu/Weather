92d8c2eaee4d739168641ad311f41adafe1a9dff
Pietro <pietro.tamburini@maxwell.mi.it>
2020-12-04 23:14:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,38 @@ function getWeather(use){
     var lon = $('#lon').val();
     
     if(use === "byName" && cityName === ""){
-        alert("inserire una cittá");
+        $('#SearchBarCity').css("animation", "shake 0.5s");
+        $('#cityName').focus();
+        setTimeout(() => { $('#SearchBarCity').css("animation", "none");}, 500);
     }else if(use === "coordinates" && (lat === "" || lon === "")){
-        alert("Inserire le coordinate mancanti");
+        $('#SearchBarCoordinates').css("animation", "shake 0.5s");
+        if(lat === "" && lon === ""){
+            $('#lat').focus();
+        }else if (lat !== "" && lon === ""){
+            $('#lon').focus();
+        }
+        setTimeout(() => { $('#SearchBarCoordinates').css("animation", "none");}, 500);
     }else{
-        if(cityName !== ""){
+        if(use === "byName"){
             var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=84fa0ddce2495b1f04851902133c2e3b';
         }else{
             var apiCall = 'http://api.openweathermap.org/data/2.5/weather?lat='+ lat +'&lon='+ lon +'&appid=84fa0ddce2495b1f04851902133c2e3b';
         }
-        $.getJSON(apiCall, weatherCallback).fail( function() { alert("error");});
+        $.getJSON(apiCall, weatherCallback).fail( function() { 
+            if(use === "byName"){
+                $('#SearchBarCity').css("animation", "shake 0.5s");
+                setTimeout(() => { $('#SearchBarCity').css("animation", "none");}, 500);
+                $('#cityName').attr("placeholder","not found, retry...");
+                $('#cityName').val("");
+            }else{
+                $('#SearchBarCoordinates').css("animation", "shake 0.5s");
+                setTimeout(() => { $('#SearchBarCoordinates').css("animation", "none");}, 500);
+                $('#lat').attr("placeholder","not");
+                $('#lon').attr("placeholder","found");
+                $('#lat').val("");
+                $('#lon').val("");
+            }
+        });
     }
     
     
@@ -86,6 +108,7 @@ function nome(){
     cityName.fadeTo("slow", 1);
     $('#SearchBarCity').css("box-shadow", "0 0 7px #ffffffab");
     $('#SearchBarCoordinates').css("box-shadow", "none");
+    $('#cityName').attr("placeholder", "Insert name of city");
 }
 
 function coordinates(){
@@ -100,6 +123,8 @@ function coordinates(){
     lon.fadeTo("slow", 1);
     $('#SearchBarCoordinates').css("box-shadow", "0 0 7px #ffffffab");
     $('#SearchBarCity').css("box-shadow", "none");
+    $('#lat').attr("placeholder", "latitude");
+    $('#lon').attr("placeholder", "longitude");
 }
 
 function check(use){
